7a34dd153184f36b513f8936539e774115376dfa
Akshat Jaimini <destrex271@gmail.com>
2022-07-10 13:00:21 +0530

    Approaches added

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,5 +1,12 @@
-import pytest
+from pytest_factoryboy import register
 from .factories import SongFactory
+import pytest
+
+# Register Approach
+
+register(SongFactory)
+
+# pytest.fixture() Approach
 
 @pytest.fixture
 def songs():
